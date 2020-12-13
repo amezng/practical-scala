6579a0b0e1e4b7e7a2b9ae6c61c105897c551d17
Shad Amez <shad.amezng@gmail.com>
2020-12-13 15:56:38 +0530

    Test the file being read

--- scripts/ibm-deploy.sh
+++ scripts/ibm-deploy.sh
@@ -4,5 +4,6 @@ files=`find $1 -maxdepth 3 -type f -not -path '*/\.*' | sort`
 
 for file in $files
 do 
-  ibmcloud cos upload --bucket $IBM_CLOUD_STATIC_BUCKET --key ${file:6} --file $file
+  echo "File to be uploaded: $file "
+  #ibmcloud cos upload --bucket $IBM_CLOUD_STATIC_BUCKET --key ${file:6} --file $file
 done
