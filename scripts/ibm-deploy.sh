13d94f7b155d44b099c9184caf2eaeee0092aec5
Shad Amez <shad.amezng@gmail.com>
2020-12-13 16:13:38 +0530

    Fix for the bucket name in the script

--- scripts/ibm-deploy.sh
+++ scripts/ibm-deploy.sh
@@ -4,5 +4,5 @@ files=`find $1 -maxdepth 3 -type f -not -path '*/\.*' | sort`
 
 for file in $files
 do 
-  ibmcloud cos upload --bucket $IBM_CLOUD_STATIC_BUCKET --key ${file:6} --file $file
+  ibmcloud cos upload --bucket $BUCKET_NAME --key ${file:6} --file $file
 done
